60b4d55c39f5eab3bf851cec00fca9243c6c9165
kigongovincent <kigongovincent81@gmail.com>
2026-07-25 00:09:52 +0300

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ouo5cqtjp1"
+    "revision": "0.3kbhl5jio6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
