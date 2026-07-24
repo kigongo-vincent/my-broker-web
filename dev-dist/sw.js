1f8d01bd1dd70d33e163d95a344eb4ff084cfc6a
kigongovincent <kigongovincent81@gmail.com>
2026-07-25 00:17:50 +0300

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a1bfmv08oug"
+    "revision": "0.kip0ft88esg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/pages/tabs/home/Search.tsx
+++ src/components/pages/tabs/home/Search.tsx
@@ -35,7 +35,7 @@ const Search = ({ filter, className, value, handleSubmit, ...attr }: Props) => {
     return (
         <form
             onSubmit={onFormSubmit}
-            className="bg-pale w-full rounded-full h-17 flex gap-3 items-center pl-6 pr-4">
+            className="bg-pale w-full rounded-full h-17 flex gap-3 items-center px-6 ">
             <Lineicons icon={Search1Outlined} className="h-8 min-h-8 min-w-8 w-8" />
             <input type="text" value={value} placeholder="search for rentals" className={`flex-1 outline-0 ${className}`} {...attr} />
             {
