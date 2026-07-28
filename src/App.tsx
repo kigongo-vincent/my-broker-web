0ddb4945b2634d8defdbf24b29e4755092ea0572
kigongovincent <kigongovincent81@gmail.com>
2026-07-28 18:28:09 +0300

    fix onboarding

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,7 @@ const App = () => {
   }, [data])
 
   const handleLookingForRental = () => {
+    setCompleteOnBoarding?.(false)
     navigate("/chat-filter")
   }
 
@@ -64,6 +65,7 @@ const App = () => {
   }
 
   const handleWantsToUpload = () => {
+    setCompleteOnBoarding?.(false)
     navigate("/upload")
   }
 
